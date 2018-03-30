119d835f964188f66e6257d05fbe7491c94aca6a
Alissa Niewiadomski <alissaaadomski@gmail.com>
2018-03-29 21:42:27 -0700

    modal separated into its own file

--- src/containers/modal.js
+++ src/containers/modal.js
@@ -1,2 +1,51 @@
 import React, { Component } from 'react';
 import { Button, Modal } from 'react-bootstrap';
+
+import { connect } from 'react-redux';
+import { addTransaction, closeModal } from '../actions/index.js';
+
+class TransactionModal extends Component {
+  constructor(props) {
+    super(props);
+    this.addTransaction = this.addTransaction.bind(this);
+  }
+
+  addTransaction() {
+    this.props.addTransaction("hi");
+  }
+
+  render() {
+    return(
+      <Modal show={this.props.modalState} onHide={() => this.props.closeModal()}>
+        <Modal.Header>
+          <Modal.Title>New Transaction</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button bsStyle="info" onClick={this.addTransaction}>Add Transaction</Button>
+          <Button onClick={() => this.props.closeModal()}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    modalState: state.modal.open
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addTransaction: (transaction) => dispatch(addTransaction(transaction)),
+    closeModal: () => dispatch(closeModal())
+  };
+};
+
+export default TransactionModal = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TransactionModal);
--- src/containers/sidebar.js
+++ src/containers/sidebar.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import { Col, Nav, NavItem, Button, Modal } from 'react-bootstrap';
+import { Col, Nav, NavItem, Button } from 'react-bootstrap';
 
 import { connect } from 'react-redux';
-import { addTransaction, closeModal, openModal } from '../actions/index.js';
+import { closeModal, openModal } from '../actions/index.js';
+
+import TransactionModal from './modal.js';
 
 class Sidebar extends Component {
-  constructor(props) {
-    super(props);
-    this.addTransaction = this.addTransaction.bind(this);
-  }
-
-  addTransaction() {
-    this.props.addTransaction("hi");
-  }
-
   render() {
     return(
       <Col sm={2} className="sidebar">
@@ -22,34 +15,18 @@ class Sidebar extends Component {
           <NavItem>Budgets</NavItem>
         </Nav>
         <Button className="button-add" bsStyle="info" onClick={() => this.props.openModal()}>Add Transaction</Button>
-        <Modal show={this.props.modalState} onHide={() => this.props.closeModal()}>
-          <Modal.Header>
-            <Modal.Title>New Transaction</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-
-          </Modal.Body>
-          <Modal.Footer>
-            <Button bsStyle="info" onClick={this.addTransaction}>Add Transaction</Button>
-            <Button onClick={() => this.props.closeModal()}>Close</Button>
-          </Modal.Footer>
-        </Modal>
+        <TransactionModal />
       </Col>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return {
-    transactions: state.transactionData,
-    modalState: state.modal.open
-  };
+  return { };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    addTransaction: (transaction) => dispatch(addTransaction(transaction)),
-    closeModal: () => dispatch(closeModal()),
     openModal: () => dispatch(openModal())
   };
 };
