ea9b3a0c9da4adb56cffb265287aeb5a040c6535
Alissa Niewiadomski <alissaaadomski@gmail.com>
2018-03-30 17:33:28 -0700

    configured redux persist

--- src/app.js
+++ src/app.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
 import logger from 'redux-logger';
 
 import reducers from './reducers/index.js';
 
 import App from './containers/App.js';
 
-let store = compose(applyMiddleware(logger))(createStore)(reducers);
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+let store = compose(applyMiddleware(logger))(createStore)(persistedReducer);
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'));
